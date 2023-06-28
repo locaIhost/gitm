480dd9a2755d94cef5bcb6617d1ebedb2705e616
locaIhost <76544214+locaIhost@users.noreply.github.com>
2023-06-28 16:48:03 +0200

    feat: added logic for adding and removing key value

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#include "headers/find.h"
+#include "headers/empty.h"
+#include "headers/putin.h"
+
 int main()
 {
+    int state_sl = sleuth();
+    if (state_sl == 0) {
+        empty();    
+    } else {
+        putin();
+    }
 }
